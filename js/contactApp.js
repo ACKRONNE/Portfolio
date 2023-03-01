27148cec5cbb1c989c48ce75673e3883f648d6f5
ACKRONNE <ACKRONNE@GMAIL.COM>
2023-03-01 12:34:27 -0400

    Contact adjustment

--- js/contactApp.js
+++ js/contactApp.js
@@ -1,4 +1,4 @@
-// Get the button:
+// Back to top button
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -16,4 +16,28 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+//Copy to clickboard
+const text = document.querySelector(".content h2");
+const popup = document.querySelector(".popup");
+
+text.addEventListener("click", () => {
+  popup.classList.add("active");
+  copyToClipBoard();
+});
+popup.addEventListener("animationend", () => {
+  popup.classList.remove("active");
+});
+
+function copyToClipBoard() {
+  const textarea = document.createElement("textarea");
+  textarea.setAttribute("readonly", "");
+  textarea.value = text.innerText;
+  textarea.style.position = "absolute";
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
 }
